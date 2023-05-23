8fba7142ecf034a5af322b6ccee06d58e775f2b2
Matheus Vieira <vieira.matheusf13@gmail.com>
2023-05-23 16:50:51 -0300

    CustomerResource implementation

---
+++ src/main/kotlin/edu/mfvp/credit/application/system/controller/CustomerResource.kt
@@ -0,0 +1,49 @@
+package edu.mfvp.credit.application.system.controller
+
+import edu.mfvp.credit.application.system.dto.CustomerDto
+import edu.mfvp.credit.application.system.dto.CustomerUpdateDto
+import edu.mfvp.credit.application.system.dto.CustomerView
+import edu.mfvp.credit.application.system.entity.Customer
+import edu.mfvp.credit.application.system.service.impl.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/customers")
+class CustomerResource(
+    private val customerService: CustomerService
+) {
+
+    @PostMapping
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
+        val savedCustomer = customerService.save(customerDto.toEntity())
+        return "Customer ${savedCustomer.email} saved!"
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
+
+    @PatchMapping
+    fun updateCustomer(@RequestParam(value = "customerId") id: Long,
+                       @RequestBody customerUpdateDto: CustomerUpdateDto): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        val customerToUpdate = customerUpdateDto.toEntity(customer)
+        val customerUpdate: Customer = this.customerService.save(customerToUpdate)
+        return CustomerView(customerUpdate)
+
+    }
+
+}
---
+++ src/main/kotlin/edu/mfvp/credit/application/system/dto/CustomerDto.kt
@@ -0,0 +1,31 @@
+package edu.mfvp.credit.application.system.dto
+
+import edu.mfvp.credit.application.system.entity.Address
+import edu.mfvp.credit.application.system.entity.Customer
+import java.math.BigDecimal
+
+data class CustomerDto(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val password: String,
+    val zipCode: String,
+    val street: String
+) {
+
+    fun toEntity(): Customer = Customer (
+        firstName = this.firstName,
+        lastName = this.lastName,
+        cpf = this.cpf,
+        income = this.income,
+        email = this.email,
+        password = this.password,
+        address = Address(
+            zipCode = this.zipCode,
+            street = this.street
+        )
+    )
+
+}
